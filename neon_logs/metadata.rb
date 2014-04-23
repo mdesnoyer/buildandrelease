46ddbfd151cec7f889699dcda38a5bcf5af9d708
Mark Desnoyer <desnoyer@neon-lab.com>
2014-04-22 18:02:09 -0700

    Add metadata

---
+++ neon_logs/metadata.rb
@@ -0,0 +1,12 @@
+name             'neon_logs'
+maintainer       'Neon Labs'
+maintainer_email 'ops@neon-lab.com'
+description      'Installs flume log handling'
+license          'Proprietary - All Rights Reserved'
+version          '0.9.0'
+
+depends 'apt'
+depends 'hadoop', ">= 1.0.4"
+depends 'java'
+
+supports ubuntu
