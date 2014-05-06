437e1e6a472de6013b287b3e6f185d67f4e4d97e
Mark Desnoyer <desnoyer@neon-lab.com>
2014-05-06 14:30:22 -0700

    Added default urls for sieging

---
+++ neon/attributes/siege.rb
@@ -0,0 +1,1 @@
+default[:neon][:siege][:trackserver_host] = "trackserver-test-691751517.us-east-1.elb.amazonaws.com"
---
+++ neon/recipes/siege.rb
@@ -0,0 +1,20 @@
+package 'siege'
+
+if node[:opsworks][:activity] == 'configure' then
+  template /home/ubuntu/.siegerc do
+    source "siegerc.erb"
+    owner "ubunutu"
+    group "ubuntu"
+    mode "0644"
+    variables({})
+  end
+
+  template "/home/ubuntu/urls.txt" do
+    source "trackserver_urls.txt.erb"
+    owner "ubunutu"
+    group "ubuntu"
+    mode "0644"
+    variables({:host => node[:neon][:siege][:trackserver_host]})
+  end
+
+end
