875b365a1e2184b5de7ac7b886382d9f2012dac4
Sunil Mallya <mallya@neon-lab.com>
2014-07-08 10:49:39 -0700

    add system configs

--- neon/attributes/imageservingplatform.rb
+++ neon/attributes/imageservingplatform.rb
@@ -20,3 +20,23 @@ force_default[:nginx][:realip][:real_ip_recursive] = "on"
 force_default[:nginx]['worker_processes'] = 1
 force_default[:nginx]['worker_connections'] = 12000
 force_default[:nginx]['worker_rlimit_nofile'] = 200000
+
+# System configs
+
+force_default[:opsworks_initial_setup][:sysctl]['net.core.somaxconn'] = 10240     
+force_default[:opsworks_initial_setup][:sysctl]['net.core.netdev_max_backlog'] = 10240
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_max_syn_backlog'] = 2048 # 1024
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_fin_timeout'] = 15 
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_keepalive_time'] = 100
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_max_orphans'] = 131072
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_tw_reuse'] = 1   
+
+# Allow a high number of timewait sockets
+force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_max_tw_buckets'] = 2000000
+
+# Wait a maximum of 5 * 2 = 10 seconds in the TIME_WAIT state after a FIN, to handle
+# any remaining packets in the network. 
+#force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.netfilter.ip_conntrack_tcp_timeout_time_wait'] = 10   
+
+# Determines the wait time between isAlive interval probes (reduce from 75 sec to 15)
+#force_default[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_keepalive_intvl'] = 15   
