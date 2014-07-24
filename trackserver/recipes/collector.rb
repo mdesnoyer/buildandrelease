51cd8e0553ebcdf1b48c210b45475f37918d2a0a
Mark Desnoyer <desnoyer@neon-lab.com>
2014-07-24 15:24:09 -0700

    trackserver collector uses smaller config recipe

--- trackserver/recipes/collector.rb
+++ trackserver/recipes/collector.rb
@@ -11,5 +11,9 @@ node.default[:neon_logs][:flume_streams][:clicklog_collector] = \
                           "bzip2",
                           'org.apache.flume.sink.hdfs.AvroEventSerializer$Builder')
 
-include_recipe "neon_logs::flume_core"
+if node[:opsworks][:activity] == "config" then
+  include_recipe "neon_logs::flume_core_config"
+else
+  include_recipe "neon_logs::flume_core"
+end
   
