822a8db08314cec05673aef6fd6595c870f1870a
Sunil Mallya <mallya@neon-lab.com>
2014-12-16 13:35:55 -0800

    add hostname entry

--- hbase/recipes/default.rb
+++ hbase/recipes/default.rb
@@ -8,11 +8,13 @@ hostsfile_entry '127.0.1.1' do
     action :nothing
     subscribes :remove, "template[/etc/hosts]", :immediately
 end
+
 # Add the entry for the ipaddress
-#hostsfile_entry "#{node['ipaddress']}" do 
-#    hostname "#{node['hostname']}" 
-#    action :create
-#end
+hostsfile_entry "#{node['ipaddress']}" do 
+    hostname "#{node['hostname']}" 
+    action :nothing
+    subscribes :create, "hostsfile_entry[127.0.1.1]", :immediately
+end
 
 # install java
 include_recipe "java::default"
