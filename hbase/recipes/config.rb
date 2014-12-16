d31fe8e0fc61e70b0e69a12cfb95bd53d0f34e4a
Sunil Mallya <mallya@neon-lab.com>
2014-12-15 18:36:25 -0800

    comment hostfile mgmt

--- hbase/recipes/config.rb
+++ hbase/recipes/config.rb
@@ -1,10 +1,10 @@
 # Remove the localhost entry from /etc/hosts
-hostsfile_entry '127.0.1.1' do 
-    action :remove
-end
+#hostsfile_entry '127.0.1.1' do 
+#    action :remove
+#end
 
 # Add the entry for the ipaddress
-hostsfile_entry "#{node['ipaddress']}" do
-    hostname "#{node['hostname']}"                                
-    action :create
-end
+#hostsfile_entry "#{node['ipaddress']}" do
+#    hostname "#{node['hostname']}"                                
+#    action :create
+#end
