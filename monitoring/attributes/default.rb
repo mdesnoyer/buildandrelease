f633a98df0f8b947de6f17690f7aba07d2cb4866
Mark Desnoyer <desnoyer@neon-lab.com>
2015-04-10 13:25:44 -0700

    Add config parameters to monitoring

--- monitoring/attributes/default.rb
+++ monitoring/attributes/default.rb
@@ -7,6 +7,12 @@ default[:monitoring][:log_file] = "#{node[:monitoring][:log_dir]}/monitoring.log
 default[:monitoring][:video_db_fallbackhost] = "redis1"
 default[:monitoring][:video_db_layer] = "redis"
 
+default[:monitoring][:isp_layer] = "neonisp"
+default[:monitoring][:cmsapi_layer] = "cmsapi"
+default[:monitoring][:account] = "159"
+default[:monitoring][:api_key] = "3yd7b8vmrj67b99f7a8o1n30"
+default[:monitoring][:sleep] = 10
+
 # Specify the repos to user
 default[:neon][:repos]["monitoring"] = true
 default[:neon][:repos]["core"] = true
--- monitoring/recipes/config.rb
+++ monitoring/recipes/config.rb
@@ -19,6 +19,12 @@ video_db_host = get_host_in_layer(node[:monitoring][:video_db_layer],
                                   node[:monitoring][:video_db_fallbackhost])
 Chef::Log.info("Connecting to video db at #{video_db_host}")
 
+# Find the isp host
+isp_host = get_host_in_layer(node[:monitoring][:isp_layer], nil)
+
+# Find the cmsapi
+cmsapi_host = get_host_in_layer(node[:monitoring][:cmsapi_layer], nil)
+
 repo_path = get_repo_path("monitoring")
 
 template node[:monitoring][:config] do
@@ -35,5 +41,10 @@ template node[:monitoring][:config] do
               :carbon_host => node[:neon][:carbon_host],
               :carbon_port => node[:neon][:carbon_port],
               :flume_log_port => node[:neon_logs][:json_http_source_port],
+              :cmsapi_host => cmsapi_host,
+              :isp_host => isp_host,
+              :account => node[:monitoring][:account],
+              :api_key => node[:monitoring][:api_key],
+              :sleep => node[:monitoring][:sleep]
             })
 end
