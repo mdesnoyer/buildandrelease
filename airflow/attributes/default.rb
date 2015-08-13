ca14c15275d28ad72ed513986ae09df3b4bfdcad
Robb Wagoner <robb.wagoner@gmail.com>
2015-08-13 08:17:20 -0700

    it is ruby, afterall

--- airflow/attributes/default.rb
+++ airflow/attributes/default.rb
@@ -22,10 +22,10 @@ default[:airflow][:db_port] = 3306
 default[:airflow][:db_name] = "airflow"
 
 # SMTP Mail for task failures and SLA violations
-default[:airflow][:smtp_user] = AKIAJZLS2HPKH33MY5RA
+default[:airflow][:smtp_user] = "AKIAJZLS2HPKH33MY5RA"
 default[:airflow][:smtp_password] = "AtpM77bc0orv6qT+e0G3Hrazz3cs1gn9Kk9TWZF+A19j"
-default[:airflow][:smtp_host] = email-smtp.us-east-1.amazonaws.com
+default[:airflow][:smtp_host] = "email-smtp.us-east-1.amazonaws.com"
 default[:airflow][:smtp_port] = 25
-default[:airflow][:smtp_from] = ops@neon-lab.com
+default[:airflow][:smtp_from] = "ops@neon-lab.com"
 
 default[:airflow][:version] = "1.3.0"
