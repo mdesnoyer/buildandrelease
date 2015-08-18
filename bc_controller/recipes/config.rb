931b834ab247cf41cf72ae687f404d50f910c5b9
Mark Desnoyer <desnoyer@neon-lab.com>
2015-08-18 14:21:19 -0700

    Fix bug finding the cmsapi server in the bc controller

--- bc_controller/recipes/config.rb
+++ bc_controller/recipes/config.rb
@@ -19,13 +19,13 @@ Chef::Log.info("Connecting to video db at #{video_db_host}")
 
 # Find Mastermind
 Chef::Log.info "Looking for the Mastermind in layer: #{node[:bc_controller][:mastermind_layer]}"
-mastermind_host = get_host_in_layer(node[:bc_controller][:mastermind],
+mastermind_host = get_host_in_layer(node[:bc_controller][:mastermind_layer],
                                     node[:bc_controller][:mastermind_fallbackhost])
 Chef::Log.info("Connecting to mastermind at #{mastermind_host}")
 
 # Find Services/ CMS API server 
 Chef::Log.info "Looking for the CMS API in layer: #{node[:bc_controller][:cmsapi_layer]}"
-cmsapi_host = get_host_in_layer(node[:bc_controller][:cmsapi],
+cmsapi_host = get_host_in_layer(node[:bc_controller][:cmsapi_layer],
                                 node[:bc_controller][:cmsapi_fallbackhost])
 Chef::Log.info("Connecting to cmsapi at #{cmsapi_host}")
 
