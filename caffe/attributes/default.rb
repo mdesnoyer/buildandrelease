286de8d06a23170662351955f6196d30009daf13
Nick <dufour@neon-lab.com>
2015-09-09 13:22:44 -0700

    attempting to make glog via tarball as in libpvx and hhvm-cookbook

--- caffe/attributes/default.rb
+++ caffe/attributes/default.rb
@@ -3,11 +3,14 @@ include_attribute "neon::default"
 default["caffe"]["software_dir"] = "#{node[:neon][:home]}/caffe"
 default["caffe"]["local_user"] = "ubuntu"
 default["caffe"]["local_group"] = "ubuntu"
-default["caffe"]["cudnn_tarball_name_wo_tgz"] = "https://neon-dependencies.s3.amazonaws.com/cudnn-7.0-linux-x64-v3.0-rc"
+default["caffe"]["remote_dir"] = "https://neon-dependencies.s3.amazonaws.com"
+default["caffe"]["cudnn_tarball_name_wo_tgz"] = "cudnn-7.0-linux-x64-v3.0-rc"
+default["caffe"]["glog_tarball_name_wo_tgz"] = "glog-0.3.3"
+
 #default["caffe"]["glog_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/libgoogle-glog-dev_0.3.4-0.1+b1_amd64"
-default["caffe"]["glog_pre_deb_file"] = "https://s3-us-west-1.amazonaws.com/neon-dependencies/libgoogle-glog0v5_0.3.4-0.1%2Bb1_amd64"
-default["caffe"]["glog_deb_file"] = "https://s3-us-west-1.amazonaws.com/neon-dependencies/libgoogle-glog-dev_0.3.4-0.1%2Bb1_amd64"
-default["caffe"]["lmdb_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/liblmdb-dev_0.9.15-1_amd64"
-default["caffe"]["CUDA_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64"
+#default["caffe"]["glog_pre_deb_file"] = "https://s3-us-west-1.amazonaws.com/neon-dependencies/libgoogle-glog0v5_0.3.4-0.1%2Bb1_amd64"
+#default["caffe"]["glog_deb_file"] = "https://s3-us-west-1.amazonaws.com/neon-dependencies/libgoogle-glog-dev_0.3.4-0.1%2Bb1_amd64"
+#default["caffe"]["lmdb_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/liblmdb-dev_0.9.15-1_amd64"
+#default["caffe"]["CUDA_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64"
 # decide whether or not it will launch interactive tools
-default["caffe"]["interactive"] = true
+#default["caffe"]["interactive"] = true
--- caffe/recipes/default.rb
+++ caffe/recipes/default.rb
@@ -10,19 +10,24 @@ include_recipe "neon::full_py_repo"
 
 
 software_dir = node[:caffe][:software_dir]
+remote_dir = node[:caffe][:remote_dir]
 local_user = node[:caffe][:local_user]
 local_group = node[:caffe][:local_group]
-# remote filenames
+glog_filename = "#{node["caffe"]["glog_tarball_name_wo_tgz"]}.tar.gz"
 cudnn_filename = "#{node['caffe']['cudnn_tarball_name_wo_tgz']}.tgz"
-cuda_filename = "#{node['caffe']['CUDA_deb_file']}.deb"
-glog_pre_filename = "#{node['caffe']['glog_pre_deb_file']}.deb"
-glog_filename = "#{node['caffe']['glog_deb_file']}.deb"
-lmdb_filename = "#{node['caffe']['lmdb_deb_file']}.deb"
+# remote filenames
+#cuda_filename = "#{node['caffe']['CUDA_deb_file']}.deb"
+#glog_pre_filename = "#{node['caffe']['glog_pre_deb_file']}.deb"
+#glog_filename = "#{node['caffe']['glog_deb_file']}.deb"
+#lmdb_filename = "#{node['caffe']['lmdb_deb_file']}.deb"
+
+#glog-0.3.3
 # local filenames
-cuda_local_filename = "/tmp/cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64.deb"
-glog_pre_local_filename = "/tmp/libgoogle-glog0v5_0.3.4-0.1+b1_amd64.deb"
-glog_local_filename = "/tmp/libgoogle-glog-dev_0.3.4-0.1+b1_amd64.deb"
-lmdb_local_filename = "/tmp/liblmdb-dev_0.9.15-1_amd64.deb"
+#cuda_local_filename = "/tmp/cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64.deb"
+#glog_pre_local_filename = "/tmp/libgoogle-glog0v5_0.3.4-0.1+b1_amd64.deb"
+#glog_local_filename = "/tmp/libgoogle-glog-dev_0.3.4-0.1+b1_amd64.deb"
+#glog_local_filename = "/tmp/"
+#lmdb_local_filename = "/tmp/liblmdb-dev_0.9.15-1_amd64.deb"
 
 # linux headers
 package "linux-headers-#{node['os_version']}"
@@ -30,6 +35,11 @@ package "linux-headers-#{node['os_version']}"
 package "linux-image-#{node['os_version']}"
 # http://stackoverflow.com/a/26525293
 package "linux-image-extra-#{node['os_version']}"
+
+directory software_dir do
+  owner local_user
+  group local_group
+end
 
 # caffe dependencies
 package_deps = [
@@ -47,181 +57,207 @@ package_deps.each do |pkg|
   end
 end
 
-# install GLOG dependency
-
-remote_file "#{glog_pre_local_filename}" do
-  source "#{glog_pre_filename}"
-  mode 0644
-end
-
-dpkg_package "libgoogle-glog0v5" do
-  source "#{glog_pre_local_filename}"
-  action :install
-end
-
-# install GLOG
-
-remote_file "#{glog_local_filename}" do
-  source "#{glog_filename}"
-  mode 0644
-end
-
-dpkg_package "libgoogle-glog" do
-  source "#{glog_local_filename}"
-  action :install
-end
-
-# install LMDB
-
-remote_file "#{lmdb_local_filename}" do
-  source "#{lmdb_filename}"
-  mode 0644
-end
-
-dpkg_package "liblmdb-dev" do
-  source "#{lmdb_local_filename}"
-  action :install
-end
-
-# install CUDA
-
-remote_file "#{cuda_local_filename}" do
-  source "#{cuda_filename}"
-  mode 0644
-end
-
-dpkg_package "cuda" do
-  source "#{cuda_local_filename}"
-  action :install
-end
-
-# remote_file "#{cuda_local_filename}" do
-#     source "#{cuda_filename}"
-#     action :create_if_missing
-#     notifies :run, 'bash[install-cuda-repo]', :immediately
-#     owner local_user
-#     group local_group
-# end
-
-# bash 'install-cuda-repo' do
-#     action :nothing
-#     code "dpkg -i #{cuda_local_filename}"
-#     notifies :run, 'execute[apt-get update]', :immediately
-# end
-
-# execute 'install-cuda' do
-#     command "apt-get -q -y install --no-install-recommends cuda"
-# end
-
-cookbook_file "#{software_dir}/#{cudnn_filename}" do
-    source "cudnn-tarball/#{cudnn_filename}"
+# let's attempt to install glog, in the same vein as cudnn
+cookbook_file "#{software_dir}/#{glog_filename}" do
+    source "#{remote_dir}/#{glog_filename}"
     mode 0644
     owner local_user
     group local_group
 end
-execute "tar -zxf #{cudnn_filename}" do
+execute "tar -zxf #{glog_filename}" do
     cwd software_dir
-    not_if { FileTest.exists? "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}" }
+    not_if { FileTest.exists? "#{software_dir}/#{node['caffe']['glog_tarball_name_wo_tgz']}" }
     user local_user
     group local_group
 end
-execute 'cp cudnn.h /usr/local/include' do
-    cwd "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}"
-    not_if { FileTest.exists? "/usr/local/include/cudnn.h" }
+execute 'google-glog-configure' do
+    cwd "#{software_dir}/#{node['caffe']['glog_tarball_name_wo_tgz']}"
+    command './configure'
+    notifies :run, 'execute[google-glog-make]', :immediately
 end
-[ 'libcudnn_static.a', 'libcudnn.so.6.5.18' ].each do |lib|
-    execute "cp #{lib} /usr/local/lib" do
-        cwd "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}"
-        not_if { FileTest.exists? "/usr/local/lib/#{lib}" }
-    end
-end
-link "/usr/local/lib/libcudnn.so.6.5" do
-    to "/usr/local/lib/libcudnn.so.6.5.18"
-end
-link "/usr/local/lib/libcudnn.so" do
-    to "/usr/local/lib/libcudnn.so.6.5"
-end
-cudnn_installed = true
-
-# set up LD_LIBRARY_PATH
-file "/etc/ld.so.conf.d/caffe.conf" do
-  owner "root"
-  group "root"
-  content "/usr/local/cuda-7.0/targets/x86_64-linux/lib"
-  notifies :run, 'execute[ldconfig]', :immediately
-end
-execute 'ldconfig' do
-  action :nothing
+execute 'google-glog-make' do
+    command "make && make install"
+    cwd software_dir
 end
 
-# download caffe and setup initial Makefile.config
-git "#{software_dir}" do
-  repository "https://github.com/BVLC/caffe.git"
-  revision "66823b59d70097f4ccbe3631b102ef238c08535b" # master as of Sep 3, 2015
-  action :sync
-  user local_user
-  group local_group
-end
-template "#{software_dir}/Makefile.config" do
-  source "Makefile.config.erb"
-  mode 0644
-  owner local_user
-  group local_group
-  variables({
-      :cudnn_installed => cudnn_installed
-  })
-end
 
-# install python requirements
-execute 'install-python-reqs' do
-  cwd "#{software_dir}/python"
-  command "(for req in $(cat requirements.txt); do pip install $req; done) && touch /home/#{local_user}/.caffe-python-reqs-installed && chown #{local_user}:#{local_group} /home/#{local_user}/.caffe-python-reqs-installed"
-  creates "/home/#{local_user}/.caffe-python-reqs-installed"
-end
+# okay, instead we're going to try to download things 
+# install GLOG dependency
 
-# make caffe!
-execute 'build-caffe' do
-  cwd "#{software_dir}"
-  command "make all -j8"
-  creates "#{software_dir}/build"
-  user local_user
-  group local_group
-  notifies :run, 'execute[build-caffe-tests]', :immediately
-end
-execute 'build-caffe-tests' do
-  cwd "#{software_dir}"
-  command "make test -j8"
-  action :nothing
-  user local_user
-  group local_group
-  notifies :run, 'execute[build-caffe-python]', :immediately
-end
-execute 'build-caffe-python' do
-  cwd "#{software_dir}"
-  command "make pycaffe"
-  action :nothing
-  user local_user
-  group local_group
-end
-
-# # fix warning message 'libdc1394 error: Failed to initialize libdc1394' when running make runtest
-# # http://stackoverflow.com/a/26028597
-# # need to set this on each boot since the /dev links are cleared after shutdown
-# cron_d 'fix-libdc1394-warning' do
-#   predefined_value '@reboot'
-#   command 'ln -s /dev/null /dev/raw1394'
+# remote_file "#{glog_pre_local_filename}" do
+#   source "#{glog_pre_filename}"
+#   mode 0644
 # end
 
-# set path
-magic_shell_environment 'PATH' do
-  value "$PATH:#{software_dir}/build/tools"
-end
-magic_shell_environment 'PYTHONPATH' do
-  value "$PYTHONPATH:#{software_dir}/python"
-end
+# dpkg_package "libgoogle-glog0v5" do
+#   source "#{glog_pre_local_filename}"
+#   action :install
+# end
 
-install_interactive = node[:caffe][:interactive]
+# # # install GLOG
 
-if install_interactive
-    execute "pip install ipython"
-end
+# # remote_file "#{glog_local_filename}" do
+# #   source "#{glog_filename}"
+# #   mode 0644
+# # end
+
+# # dpkg_package "libgoogle-glog" do
+# #   source "#{glog_local_filename}"
+# #   action :install
+# # end
+
+# # install LMDB
+
+# remote_file "#{lmdb_local_filename}" do
+#   source "#{lmdb_filename}"
+#   mode 0644
+# end
+
+# dpkg_package "liblmdb-dev" do
+#   source "#{lmdb_local_filename}"
+#   action :install
+# end
+
+# # install CUDA
+
+# remote_file "#{cuda_local_filename}" do
+#   source "#{cuda_filename}"
+#   mode 0644
+# end
+
+# dpkg_package "cuda" do
+#   source "#{cuda_local_filename}"
+#   action :install
+# end
+
+# # remote_file "#{cuda_local_filename}" do
+# #     source "#{cuda_filename}"
+# #     action :create_if_missing
+# #     notifies :run, 'bash[install-cuda-repo]', :immediately
+# #     owner local_user
+# #     group local_group
+# # end
+
+# # bash 'install-cuda-repo' do
+# #     action :nothing
+# #     code "dpkg -i #{cuda_local_filename}"
+# #     notifies :run, 'execute[apt-get update]', :immediately
+# # end
+
+# # execute 'install-cuda' do
+# #     command "apt-get -q -y install --no-install-recommends cuda"
+# # end
+
+# ------------------ legit stuff -------------------- #
+# cookbook_file "#{software_dir}/#{cudnn_filename}" do
+#     source "cudnn-tarball/#{cudnn_filename}"
+#     mode 0644
+#     owner local_user
+#     group local_group
+# end
+# execute "tar -zxf #{cudnn_filename}" do
+#     cwd software_dir
+#     not_if { FileTest.exists? "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}" }
+#     user local_user
+#     group local_group
+# end
+# execute 'cp cudnn.h /usr/local/include' do
+#     cwd "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}"
+#     not_if { FileTest.exists? "/usr/local/include/cudnn.h" }
+# end
+# [ 'libcudnn_static.a', 'libcudnn.so.6.5.18' ].each do |lib|
+#     execute "cp #{lib} /usr/local/lib" do
+#         cwd "#{software_dir}/#{node['caffe']['cudnn_tarball_name_wo_tgz']}"
+#         not_if { FileTest.exists? "/usr/local/lib/#{lib}" }
+#     end
+# end
+# link "/usr/local/lib/libcudnn.so.6.5" do
+#     to "/usr/local/lib/libcudnn.so.6.5.18"
+# end
+# link "/usr/local/lib/libcudnn.so" do
+#     to "/usr/local/lib/libcudnn.so.6.5"
+# end
+# cudnn_installed = true
+
+# # set up LD_LIBRARY_PATH
+# file "/etc/ld.so.conf.d/caffe.conf" do
+#   owner "root"
+#   group "root"
+#   content "/usr/local/cuda-7.0/targets/x86_64-linux/lib"
+#   notifies :run, 'execute[ldconfig]', :immediately
+# end
+# execute 'ldconfig' do
+#   action :nothing
+# end
+
+# # download caffe and setup initial Makefile.config
+# git "#{software_dir}" do
+#   repository "https://github.com/BVLC/caffe.git"
+#   revision "66823b59d70097f4ccbe3631b102ef238c08535b" # master as of Sep 3, 2015
+#   action :sync
+#   user local_user
+#   group local_group
+# end
+# template "#{software_dir}/Makefile.config" do
+#   source "Makefile.config.erb"
+#   mode 0644
+#   owner local_user
+#   group local_group
+#   variables({
+#       :cudnn_installed => cudnn_installed
+#   })
+# end
+
+# # install python requirements
+# execute 'install-python-reqs' do
+#   cwd "#{software_dir}/python"
+#   command "(for req in $(cat requirements.txt); do pip install $req; done) && touch /home/#{local_user}/.caffe-python-reqs-installed && chown #{local_user}:#{local_group} /home/#{local_user}/.caffe-python-reqs-installed"
+#   creates "/home/#{local_user}/.caffe-python-reqs-installed"
+# end
+
+# # make caffe!
+# execute 'build-caffe' do
+#   cwd "#{software_dir}"
+#   command "make all -j8"
+#   creates "#{software_dir}/build"
+#   user local_user
+#   group local_group
+#   notifies :run, 'execute[build-caffe-tests]', :immediately
+# end
+# execute 'build-caffe-tests' do
+#   cwd "#{software_dir}"
+#   command "make test -j8"
+#   action :nothing
+#   user local_user
+#   group local_group
+#   notifies :run, 'execute[build-caffe-python]', :immediately
+# end
+# execute 'build-caffe-python' do
+#   cwd "#{software_dir}"
+#   command "make pycaffe"
+#   action :nothing
+#   user local_user
+#   group local_group
+# end
+
+# # # fix warning message 'libdc1394 error: Failed to initialize libdc1394' when running make runtest
+# # # http://stackoverflow.com/a/26028597
+# # # need to set this on each boot since the /dev links are cleared after shutdown
+# # cron_d 'fix-libdc1394-warning' do
+# #   predefined_value '@reboot'
+# #   command 'ln -s /dev/null /dev/raw1394'
+# # end
+
+# # set path
+# magic_shell_environment 'PATH' do
+#   value "$PATH:#{software_dir}/build/tools"
+# end
+# magic_shell_environment 'PYTHONPATH' do
+#   value "$PYTHONPATH:#{software_dir}/python"
+# end
+
+# install_interactive = node[:caffe][:interactive]
+
+# if install_interactive
+#     execute "pip install ipython"
+# end
