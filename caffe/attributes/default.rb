708e1b198ed92de27f042cd80b0cc9bddbf3cbf4
Nick <dufour@neon-lab.com>
2015-09-09 19:18:26 -0700

    not sure what the bug is

--- caffe/attributes/default.rb
+++ caffe/attributes/default.rb
@@ -9,7 +9,7 @@ default["caffe"]["local_group"] = "ubuntu"
 default["caffe"]["cudnn_tarball_name_wo_tgz"] = "cudnn-7.0-linux-x64-v3.0-rc"
 
 default["caffe"]["glog_tarball_name_wo_tgz"] = "glog-0.3.3"
-default["caffe"]["CUDA_deb_file"] = "https://neon-dependencies.s3.amazonaws.com/cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64"
+default["caffe"]["CUDA_deb_file"] = "cuda-repo-ubuntu1204-7-0-local_7.0-28_amd64"
 
 default["caffe"]["lmdb_build_dir"] = "#{Chef::Config[:file_cache_path]}/lmbd"
 default["caffe"]["lmdb_git_repository"] = "https://github.com/LMDB/lmdb.git"
