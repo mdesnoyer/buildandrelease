941dc84a437ae7ce822c9cefe1b826a72ccc8759
Kevin Fenger <fenger@neon-lab.com>
2015-10-29 08:43:11 -0700

    cms api auth chef stuff

---
+++ cmsapiauth/recipes/config.rb
@@ -0,0 +1,67 @@
+# set the flume sources
+node.default[:neon_logs][:flume_streams][:cmsapi_logs] = 
+  get_jsonagent_config(node[:neon_logs][:json_http_source_port],
+                       "cmsapi")
+
+node.default[:neon_logs][:flume_streams][:cmsapi_flume_logs] = \
+  get_fileagent_config("#{get_log_dir()}/flume.log",
+                       "cmsapi-flume")
+
+if node[:opsworks][:activity] == "config" then
+  include_recipe "neon_logs::flume_core_config"
+else
+  include_recipe "neon_logs::flume_core"
+end
+
+db_host = get_master_cmsdb_ip()
+Chef::Log.info("Connecting to db at #{db_host}")
+
+# Write the configuration file for CMS API AUTH 
+template node[:cmsapi][:config] do
+  source "cmsapiauth.conf.erb"
+  owner "cmsapi"
+  group "cmsapi"
+  mode "0644"
+  variables({
+              :cmsapiauth_port => node[:cmsapiauth][:port],
+              :log_file => node[:cmsapi][:log_file],
+              :access_log_file => node[:cmsapiauth][:access_log_file],
+              :db_port => node[:cmsdb][:master_port],
+              :db_host => db_host,
+              :carbon_host => node[:neon][:carbon_host],
+              :carbon_port => node[:neon][:carbon_port],
+              :flume_log_port => node[:neon_logs][:json_http_source_port],
+            })
+end
+
+cmsapiauth_exists = File.exists?("/etc/init/cmsapiauth.conf")
+
+if cmsapiauth_exists then
+  # Specify the service for chef so that they can be restarted.
+  service "cmsapiauth" do
+    provider Chef::Provider::Service::Upstart
+    supports :status => true, :restart => true, :start => true, :stop => true
+    action :nothing
+  end
+
+  service "nginx" do
+    action :nothing
+  end
+end
+
+include_recipe "neon-nginx::commons_dir"
+
+# Write the configuration for nginx
+template "#{node[:nginx][:dir]}/conf.d/cmsapiauth.conf" do
+  source "cmsapiauth_nginx.conf.erb"
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  mode "0644"
+  variables({
+              :service_port => node[:cmsapiauth][:port], 
+              :frontend_port => 80 
+            })
+  if cmsapiauth_exists
+    notifies :reload, 'service[nginx]', :delayed
+  end
+end
---
+++ cmsapiauth/recipes/default.rb
@@ -0,0 +1,115 @@
+# This recipe installs the neon cmsapi process
+
+include_recipe "neon::default"
+
+# Setup collecting system metrics
+include_recipe "neon::system_metrics"
+
+# Create a cmsapi user
+user "cmsapi" do
+  action :create
+  system true
+  shell "/bin/false"
+end
+
+include_recipe "cmsapiauth::config"
+
+# Install the python dependencies
+
+# Make directories 
+directory node[:cmsapiauth][:log_dir] do
+  user "neon"
+  group "neon"
+  mode "0755"
+end
+file node[:cmsapiauth][:log_file] do
+  user "neon"
+  group "neon"
+  mode "0644"
+end
+ 
+node[:deploy].each do |app_name, deploy|
+  if app_name == "cmsapiauth" then
+    Chef::Log.info("Deploying app base installs.")
+    include_recipe "neon::full_py_repo"
+    include_recipe "neon-nginx::default"
+    template "/etc/init/nginx-email.conf" do
+      source "mail-on-restart.conf.erb"
+      cookbook "neon"
+      owner "root"
+      group "root"
+      mode "0644"
+      variables({
+                  :service => "nginx",
+                  :host => node[:hostname],
+                  :email => node[:neon][:ops_email],
+                  :log_file => "#{node[:nginx][:log_dir]}/error.log"
+                })
+    end
+
+    service "nginx" do
+      action [:enable, :start]
+    end
+
+    repo_path = get_repo_path(app_name)
+    Chef::Log.info("Deploying app #{app_name} using code path #{repo_path}")
+    template "/etc/init/cmsapiauth.conf" do
+      source "cmsapiauth_service.conf.erb"
+      owner "root"
+      group "root"
+      mode "0644"
+      variables({
+                  :neon_root_dir => "#{repo_path}",
+                  :config_file => node[:cmsapiauth][:config],
+                  :user => "neon",
+                  :group => "neon",
+                })
+    end
+    # Write a script that will send a mail when the service dies
+    template "/etc/init/cmsapiauth-email.conf" do
+      source "mail-on-restart.conf.erb"
+      cookbook "neon"
+      owner "root"
+      group "root"
+      mode "0644"
+      variables({
+                  :service => "cmsapiauth",
+                  :host => node[:hostname],
+                  :email => node[:neon][:ops_email],
+                  :log_file => node[:cmsapiauth][:log_file]
+                })
+    end
+    service "cmsapiauth" do
+      provider Chef::Provider::Service::Upstart
+      supports :status => true, :restart => true, :start => true, :stop => true
+      action [:enable, :start]
+      subscribes :restart, "git[#{repo_path}]", :delayed
+    end
+    app_tested = "#{repo_path}/TEST_DONE"
+    file app_tested do
+      user "neon"
+      group "neon"
+      action :nothing
+      subscribes :delete, "bash[compile_cmsapiauth]", :immediately
+    end
+    bash "test_cmsapiauth" do
+      cwd repo_path
+      user "neon"
+      group "neon"
+      code <<-EOH
+         . enable_env
+         nosetests --exe cmsapiv2 cmsdb utils
+      EOH
+      not_if {  ::File.exists?(app_tested) }
+      notifies :restart, "service[cmsapiauth]", :delayed
+      notifies :create, "file[#{app_tested}]"
+    end
+  end 
+end
+
+if ['undeploy', 'shutdown'].include? node[:opsworks][:activity] then
+  # Turn off cmsapi
+  service "cmsapiauth" do
+    action :stop
+  end
+end
