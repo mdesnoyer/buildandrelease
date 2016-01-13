88681fc6dbdc8cd50dd2a593eb6389cfea1ed22c
Kevin Fenger <fenger@neon-lab.com>
2016-01-13 11:43:41 -0800

    nginx escape args

--- neonisp/recipes/nginx_ispmodule.rb
+++ neonisp/recipes/nginx_ispmodule.rb
@@ -1,4 +1,7 @@
 
 repo_path = get_repo_path(node[:neonisp][:app_name])
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--add-module=#{repo_path}/imageservingplatform/neon_isp"]
+  node.run_state['nginx_configure_flags'] | ["--add-module=#{repo_path}/imageservingplatform/neon_isp" 
+                                             "--add-module=#{repo_path}/imageservingplatform/ngx_devel_kit-0.2.19" 
+                                             "--add-module=#{repo_path}/imageservingplatform/set-misc-nginx-module-0.29" 
+                                            ]
