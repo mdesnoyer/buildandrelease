b521d5d85a3200aac5ccd6c5555f3fd56dfe1cf0
Mark Desnoyer <desnoyer@neon-lab.com>
2016-01-29 14:22:33 -0800

    opsworks changes for new benchmarking system

--- monitoring/attributes/default.rb
+++ monitoring/attributes/default.rb
@@ -5,12 +5,12 @@ include_attribute "cmsdb::default"
 default[:monitoring][:log_dir] = "#{node[:neon][:log_dir]}/monitoring"
 default[:monitoring][:config] = "#{node[:neon][:config_dir]}/monitoring.conf"
 default[:monitoring][:log_file] = "#{node[:monitoring][:log_dir]}/monitoring.log"
-default[:monitoring][:isp_layer] = "neonisp"
-default[:monitoring][:cmsapi_layer] = "cmsapi"
 default[:monitoring][:account] = "159"
 default[:monitoring][:api_key] = "3yd7b8vmrj67b99f7a8o1n30"
+default[:monitoring][:cmsapi_user] = nil
+default[:monitoring][:cmsapi_pass] = nil
 default[:monitoring][:sleep] = 10
 
-# Specify the repos to user
+# Specify the repos to use
 default[:neon][:repos]["monitoring"] = true
 default[:neon][:repos]["core"] = true
