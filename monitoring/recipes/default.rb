32b3a2922c1f122ae6676b998a7e78ec051ed292
Mark Desnoyer <desnoyer@neon-lab.com>
2016-02-01 15:41:04 -0800

    Add py dependencies

--- monitoring/recipes/default.rb
+++ monitoring/recipes/default.rb
@@ -14,38 +14,6 @@ end
 
 include_recipe "monitoring::config"
 
-# Install the python dependencies
-
-pydeps = {
-  "numpy" => "1.9.2",
-  "futures" => "2.1.5",
-  "tornado" => "4.2.1",
-  "shortuuid" => "0.3",
-  "PyYAML" => "3.10",
-  "simplejson" => "2.3.2",
-  "nose" => "1.3.0",
-  "pyfakefs" => "2.4",
-  "mock" => "1.0.1",
-  "httpagentparser" => "1.6.0",
-  "psutil" => "1.2.1",
-  "winpdb" => "1.4.8", 
-  "python-dateutil" => "2.4.2",
-  "redis" => "2.10.3",
-  "imagehash" => "0.3",
-  "poster" => "0.8.1"
-}
-
-pydeps.each do |package, vers|
-  python_pip package do
-    version vers
-    options "--no-index --find-links https://s3-us-west-1.amazonaws.com/neon-dependencies/index.html"
-  end
-end
-
-package "python-nose" do
-  :install
-end
-
 # Make directories 
 directory node[:monitoring][:log_dir] do
   user "neon"
@@ -68,15 +36,7 @@ node[:deploy].each do |app_name, deploy|
   Chef::Log.info("Deploying app #{app_name} using code path #{repo_path}")
   
   # Install the neon code
-  include_recipe "neon::repo"
-
-  # Test the trackserver
-  execute "nosetests --exe cmsdb monitoring utils" do
-    cwd "#{repo_path}"
-    user "neon"
-    action :run
-    notifies :restart, "service[benchmark_videopipeline]", :delayed
-  end
+  include_recipe "neon::full_py_repo"
 
   # Write the daemon service wrapper
   template "/etc/init/benchmark_videopipeline.conf" do
