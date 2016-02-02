1cff80504f804bbd268d18a90f93b5d8ac595e1a
Mark Desnoyer <desnoyer@neon-lab.com>
2016-02-02 14:36:15 -0800

    monitoring doesn't need an api key

--- monitoring/attributes/default.rb
+++ monitoring/attributes/default.rb
@@ -6,7 +6,6 @@ default[:monitoring][:log_dir] = "#{node[:neon][:log_dir]}/monitoring"
 default[:monitoring][:config] = "#{node[:neon][:config_dir]}/monitoring.conf"
 default[:monitoring][:log_file] = "#{node[:monitoring][:log_dir]}/monitoring.log"
 default[:monitoring][:account] = "3hr5242g1ho5jcfogz8fec53"
-default[:monitoring][:api_key] = "grtxcau33l5adas92bhtn5zu"
 default[:monitoring][:cmsapi_user] = "admin_neon_benchmark"
 default[:monitoring][:cmsapi_pass] = nil
 default[:monitoring][:auth_host] = "auth.neon-lab.com"
