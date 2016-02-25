82a9bc1865e2cbacd30459147bdbc21b942ddafa
Kevin Fenger <fenger@neon-lab.com>
2016-02-25 13:19:38 -0800

    test

--- bc_controller/recipes/config.rb
+++ bc_controller/recipes/config.rb
@@ -30,25 +30,26 @@ cmsapi_host = get_host_in_layer(node[:bc_controller][:cmsapi_layer],
 Chef::Log.info("Connecting to cmsapi at #{cmsapi_host}")
 
 # Write the configuration file 
-#template node[:bc_controller][:config] do
-#  source "bc_controller.conf.erb"
-#  owner "bc_controller"
-#  group "bc_controller"
-#  mode "0644"
-#  variables({
-#              :bc_controller_port => node[:bc_controller][:port],
-#              :video_db_host => video_db_host,
-#              :video_db_port => node[:cmsdb][:master_port],
-#              :mastermind_host => mastermind_host,
-#              :mastermind_port => node[:bc_controller][:mastermind_port],
-#              :cmsapi_host => cmsapi_host, 
-#              :cmsapi_port => node[:bc_controller][:cmsapi_port], 
-#              :log_file => node[:bc_controller][:log_file],
-#              :carbon_host => node[:neon][:carbon_host],
-#              :carbon_port => node[:neon][:carbon_port],
-#              :flume_log_port => node[:neon_logs][:json_http_source_port],
-#            })
-#end
+template node[:bc_controller][:config] do
+  Chef::Log.info("BLAM node #{node}")
+  source "bc_controller.conf.erb"
+  owner "bc_controller"
+  group "bc_controller"
+  mode "0644"
+  variables({
+              :bc_controller_port => node[:bc_controller][:port],
+              :video_db_host => video_db_host,
+              :video_db_port => node[:cmsdb][:master_port],
+              :mastermind_host => mastermind_host,
+              :mastermind_port => node[:bc_controller][:mastermind_port],
+              :cmsapi_host => cmsapi_host, 
+              :cmsapi_port => node[:bc_controller][:cmsapi_port], 
+              :log_file => node[:bc_controller][:log_file],
+              :carbon_host => node[:neon][:carbon_host],
+              :carbon_port => node[:neon][:carbon_port],
+              :flume_log_port => node[:neon_logs][:json_http_source_port],
+            })
+end
 
 template node[:bc_controller][:ingester_config] do
   source "ingester.conf.erb"
