2734d7731e52fd38d25ac20b37fe385e6495ef75
Mark Desnoyer <desnoyer@neon-lab.com>
2016-06-05 14:35:57 -0700

    Read for OpsWorks version of Aquila

--- video_client/attributes/default.rb
+++ video_client/attributes/default.rb
@@ -20,6 +20,7 @@ default[:video_client][:extra_workers] = 0
 default[:video_client][:video_temp_dir] = "/mnt/tmp1"
 default[:video_client][:video_queue_prefix] = "videojobs_priority_"
 default[:video_client][:model_autoscale_groups] = "AquilaOnDemandTest"
+default[:video_client][:request_concurrency] = 22
 
 # Specify the repos to user
 default[:neon][:repos]["video_client"] = true
