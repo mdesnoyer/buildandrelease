5666d5df9ab7dc802d4cc85f0dfc6f4a35a84f10
Mark Desnoyer <desnoyer@neon-lab.com>
2016-06-21 09:23:27 -0700

    Starting to build cloud init scripts

--- hbase/recipes/default.rb
+++ hbase/recipes/default.rb
@@ -21,14 +21,14 @@ end
 # Remove the localhost entry from /etc/hosts
 # TODO: Make this a function
 hostsfile_entry '127.0.1.1' do 
-    action :nothing
+    action :remove
     subscribes :remove, "template[/etc/hosts]", :immediately
 end
 
 # Add the entry for the ipaddress
 hostsfile_entry "#{node['ipaddress']}" do 
     hostname "#{node['hostname']}" 
-    action :nothing
+    action :create
     subscribes :create, "hostsfile_entry[127.0.1.1]", :immediately
 end
 
