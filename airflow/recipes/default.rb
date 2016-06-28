759d0bc4e68781621140620191180e509b99bdf6
nadeem86 <nadeem.ahmed86@gmail.com>
2016-06-27 22:30:51 -0700

    reset airflow db

--- airflow/recipes/default.rb
+++ airflow/recipes/default.rb
@@ -112,7 +112,7 @@ end
 # ----------------------------------------
 execute "airflow initialize db" do
   command "airflow resetdb -y"
-  user "airflow"
+  user "statsmanager"
 end
 
 if ['shutdown'].include? node[:opsworks][:activity] then
