fb1a8c4240a02ad584d732f0650feef3dc57c1d6
nadeem86 <nadeem.ahmed86@gmail.com>
2016-08-19 11:57:15 -0700

    change date to ISO & add rebase date

--- stats_manager/recipes/config.rb
+++ stats_manager/recipes/config.rb
@@ -17,7 +17,6 @@ template node[:stats_manager][:config] do
   group node[:stats_manager][:group]
   mode "0644"
   variables({ 
-              :batch_period => node[:stats_manager][:batch_period],
               :cluster_type => node[:stats_manager][:cluster_type],
               :cluster_name => node[:stats_manager][:cluster_name],
               :cluster_ip => node[:stats_manager][:cluster_public_ip],
@@ -27,11 +26,11 @@ template node[:stats_manager][:config] do
               :max_task_instances => node[:stats_manager][:max_task_instances],
               :master_instance_type => node[:stats_manager][:master_instance_type],
               :airflow_start_date => node[:stats_manager][:airflow_start_date],
+              :airflow_rebase_date => node[:stats_manager][:airflow_rebase_date],
               :notify_email => node[:stats_manager][:notify_email],
               :full_run_input_path => node[:stats_manager][:full_run_input_path],
               :n_core_instances => node[:stats_manager][:n_core_instances],
               :input_path => node[:stats_manager][:input_path],
-              :cleaned_output_path => node[:stats_manager][:cleaned_output_path],
               :cc_cleaned_path => node[:stats_manager][:cc_cleaned_path],
               :quiet_period => node[:stats_manager][:quiet_period],
               :log_file => node[:stats_manager][:log_file],
