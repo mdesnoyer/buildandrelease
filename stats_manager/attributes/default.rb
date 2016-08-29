70e2699e7dccce7a87ff4abe6a5d03be2045475a
nadeem86 <nadeem.ahmed86@gmail.com>
2016-08-29 16:54:19 -0700

    remove unused attribute

--- stats_manager/attributes/default.rb
+++ stats_manager/attributes/default.rb
@@ -33,7 +33,6 @@ default[:stats_manager][:quiet_period] = 30
 default[:stats_manager][:mr_jar] = "neon-stats-1.0-job.jar"
 default[:stats_manager][:input_path] = "s3://neon-tracker-logs-v2/v2.2"
 default[:stats_manager][:full_run_input_path] = "/*/*/*/*"
-default[:stats_manager][:output_path] = "s3://neon-tracker-logs-test-hadoop/"
 default[:stats_manager][:staging_path] = "s3://neon-tracker-logs-v2/airflow/staging"
 default[:stats_manager][:cleaned_output_path] = "s3://neon-tracker-logs-v2/airflow/cleaned"
 default[:stats_manager][:cc_cleaned_path] = "s3://neon-tracker-logs-v2/airflow/cc_cleaned"
