6d1c4bce67b50be78e07fb4a50c8eaf3e70761ad
Nate Kresge <kresge@neon-lab.com>
2016-10-31 16:25:00 -0700

    Try 2.8 ffmpeg

--- neon-opencv/recipes/install.rb
+++ neon-opencv/recipes/install.rb
@@ -91,7 +91,7 @@ end
 
 # Install ffmpeg
 node.default[:ffmpeg][:git_repository] = 'https://github.com/FFmpeg/FFmpeg.git'
-node.default[:ffmpeg][:git_revision] = 'n3.2'
+node.default[:ffmpeg][:git_revision] = 'n2.8.8'
 node.default[:ffmpeg][:compile_flags] = [
                                          "--enable-pthreads",
                                          "--enable-nonfree",
